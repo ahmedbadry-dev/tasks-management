c83d932eabd98f6b26f4c4c29a2475495439262f
ahmedbadry-dev <ahmedbadry.dev@gmail.com>
2026-04-17 16:52:59 +0200

    feat(projects): add new project creation flow with dedicated page and form
    add /project/add route and AddProjectView container
    
    implement AddProjectForm with title/description fields and CTA actions
    
    add zod schema for add-project validation rules

---
+++ features/projects/components/AddProjectForm.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+
+import { Input } from '@/shared/components/input'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { AddProjectFormSchema, TAddProjectFormSchema } from '../validations/AddProjectFormSchema';
+import { AddUserIcon, BellIcon, InviteUserIcon } from '@/shared/components/icons';
+import { Textarea } from '@/shared/components/Textarea';
+
+
+
+
+
+export const AddProjectForm = () => {
+
+    const { register, handleSubmit, formState: { errors, isSubmitting }, setError } = useForm<TAddProjectFormSchema>({
+        resolver: zodResolver(AddProjectFormSchema),
+        defaultValues: {
+            name: '',
+            description: ''
+        },
+        mode: 'onBlur'
+    })
+
+
+    const onSubmit: SubmitHandler<TAddProjectFormSchema> = async (data) => {
+        console.log(data)
+    }
+    return (
+        <div className="w-full sm:w-2xl">
+            <div className=" bg-background rounded-lg sm:shadow-card mb-20">
+                <div className=' px-6 sm:p-8 '>
+                    {/* card header */}
+                    <div className="flex items-center gap-4 pb-10 ">
+                        <div className=' hidden sm:flex justify-center items-center w-11 h-11 bg-primary-container/10 rounded-md'>
+                            <AddUserIcon className='text-primary' />
+                        </div>
+                        <div>
+                            <h1 className="heading-2">Initialize New Project</h1>
+                            <p className="type-body-md">Define the scope and foundational details of your project.</p>
+                        </div>
+                    </div>
+
+                    {/* card content */}
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <Input
+                            register={register}
+                            name="name"
+                            label="Project TITLE "
+                            type="text"
+                            placeholder="Pr"
+                            error={errors.name}
+                        />
+                        <Textarea
+                            register={register}
+                            name="description"
+                            label="Description"
+                            placeholder="Provide a high-level overview of the project's architectural objectives and key milestones..."
+                            error={errors.description}
+                            hint="Optional"
+                            rows={4}
+                            maxLength={500}
+                        />
+                        {errors.root?.message && (
+                            <p className="text-sm text-red-500 mb-4">{errors.root.message}</p>
+                        )}
+
+
+                        <div className='flex flex-col-reverse justify-between items-center   py-4 sm:flex-row '>
+                            <button
+                                type="button"
+                                className="btn btn-ghost mt-2 max-sm:w-full"
+                                disabled={isSubmitting}
+                            >
+                                {isSubmitting ? 'Canceling...' : 'Cancel'}
+                            </button>
+
+                            <button
+                                type="submit"
+                                className="btn btn-primary mt-2 max-sm:w-full "
+                                disabled={isSubmitting}
+                            >
+                                {isSubmitting ? 'Create Project...' : 'Create Project'}
+                            </button>
+                        </div>
+                    </form>
+                </div>
+                {/* card Pro Tip */}
+                <div className="flex justify-center items-center gap-2 bg-surface-low p-6 ">
+
+                    <div className='hidden sm:block'>
+                        <BellIcon />
+                    </div>
+                    <div className='flex flex-col sm:flex-row sm:items-center gap-2'>
+                        <span className='type-body-md text-[12px] font-bold '>Pro Tip</span>
+                        <p className="type-body-md text-[12px]"> You can invite project members and assign epics immediately after the initial creation process.</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
